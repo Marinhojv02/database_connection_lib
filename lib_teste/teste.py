c363a9f0d350c8879f892edc2db0ad1f9968cf4b
Carol Silva <joaovm048@gmail.com>
2024-06-22 13:49:53 -0300

    asdasd

--- lib_teste/teste.py
+++
@@ -1,6 +0,0 @@
-class teste:
-    def __init__(self):
-        pass
-
-    def testar(self):
-        print('testado chefia')
--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name='LibTestePython',
-    url='https://github.com/Marinhojv02/teste-python-lib',
-    author='Joao Victor Marinho',
-    author_email='joaovm048@gmail.com',
-    # Needed to actually package something
-    packages=['lib_teste'],
-    # Needed for dependencies
-    install_requires=[],
-    # *strongly* suggested for sharing
+    name='sua_lib',
     version='0.1',
-    # The license can be anything you like
-    license='None',
-    description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
-    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=[
+        # Lista de dependências
+    ],
+    scripts=[
+        # Scripts adicionais, se houver
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # Outras classificações
+    ],
 )
