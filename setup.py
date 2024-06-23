aad2b20f4a847c396b22cec04a3dae95ad66621c
joao marinho <joaovm048@gmail.com>
2024-06-23 11:28:34 -0300

    conexao postgres

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sua_lib',
+    name='database_connection_lib',
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        # Lista de dependências
-    ],
-    scripts=[
-        # Scripts adicionais, se houver
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        # Outras classificações
+        "sqlalchemy",
+        "psycopg2-binary"
     ],
 )
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name='sua_lib',
+#     version='0.1',
+#     packages=find_packages(),
+#     install_requires=[
+#         # Lista de dependências
+#     ],
+#     scripts=[
+#         # Scripts adicionais, se houver
+#     ],
+#     classifiers=[
+#         'Programming Language :: Python :: 3',
+#         # Outras classificações
+#     ],
+# )
