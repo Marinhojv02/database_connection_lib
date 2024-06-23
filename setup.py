ae863d4dd7a1e3c0382a27418fd0bd4f51570489
joao marinho <joaovm048@gmail.com>
2024-06-23 11:54:36 -0300

    ta funcionando agora

--- setup.py
+++ setup.py
@@ -8,22 +8,11 @@ setup(
         "sqlalchemy",
         "psycopg2-binary"
     ],
+    scripts=[
+        # Scripts adicionais, se houver
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # Outras classificações
+    ],
 )
-
-# from setuptools import setup, find_packages
-
-# setup(
-#     name='sua_lib',
-#     version='0.1',
-#     packages=find_packages(),
-#     install_requires=[
-#         # Lista de dependências
-#     ],
-#     scripts=[
-#         # Scripts adicionais, se houver
-#     ],
-#     classifiers=[
-#         'Programming Language :: Python :: 3',
-#         # Outras classificações
-#     ],
-# )
